c1eedda1d91281a900d4346497fde25e00416c87
Luke McGregor <lukiluke@xtra.co.nz>
2019-05-22 13:12:56 +1200

    Added Login

--- app/src/main/java/com/example/quizical/MainMenu.java
+++ app/src/main/java/com/example/quizical/MainMenu.java
@@ -1,6 +1,9 @@
 package com.example.quizical;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,18 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void login(View v){
+        String usernameValid = "lukemcgregs@gmail.com";
+        String passwordValid = "lukemcgregor23";
+
+        EditText username = (EditText)findViewById(R.id.editTextUN);
+        EditText password = (EditText)findViewById(R.id.editTextPW);
+
+        if(username.getText().toString().equals(usernameValid) && password.getText().toString().equals(passwordValid)) {
+            startActivity(new Intent(this, PlayScreen.class));
+        }
     }
 
 }
